a49b883c240f2d8a4e9669737f9a62e1a73468fa
chanceDira <chancedesire60@gmail.com>
2022-02-21 07:24:33 +0200

    Navbar finished

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = ({ ...otherProps }) => {
-  return (
-    <header>
-      <div>
-        <img src="https://cdn.pixabay.com/photo/2017/10/05/10/05/leaf-2818896_960_720.jpg" alt="logo" />
-      </div>
 
-      <nav>
-        <ul>
-          <li>
-            <Link>Services</Link>
-          </li>
-        </ul>
-        <li>
-          <Link>About us</Link>
-        </li>
-        <li>
-          <Link>Contact Us</Link>
-        </li>
-      </nav>
+    const [active, setActive] = useState(false)
+
+    const onClick = () => {
+        setActive(!active)
+    }
+
+  return (
+    <header className="bg-white relative">
+      <div className="max-w-7xl mx-auto flex items-center justify-between p-2.5">
+        <div className="w-14">
+          <img
+            src="https://cdn.pixabay.com/photo/2017/10/05/10/05/leaf-2818896_960_720.jpg"
+            alt="logo"
+            className="w-full"
+          />
+        </div>
+
+        <div 
+            onClick={onClick}
+            className={`
+            md:hidden uppercase
+            `}
+        >
+            Menu
+        </div>
+
+        <nav className={`
+            ${!active && 'hidden'}
+            absolute flex flex-col bg-white top-full w-full left-0 z-20
+            md:static md:w-auto md:flex-row md:flex
+        `}>
+          <ul className="md:flex md:flex-row">
+            <li className="list-none md:mr-5">
+              <Link className="flex w-full text-base uppercase hover:text-red-600 cursor-pointer pt-2.5 px-2.5">Services</Link>
+            </li>
+            <li className="list-none md:mr-5">
+              <Link className="flex w-full text-base uppercase hover:text-red-600 cursor-pointer pt-2.5 px-2.5">About Us</Link>
+            </li>
+            <li className="list-none md:mr-5">
+              <Link className="flex w-full text-base uppercase hover:text-red-600 cursor-pointer pt-2.5 px-2.5">Contact Us</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
